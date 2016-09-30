c578d3b69ab4d69423a84fc2fda4f90534fac8fb
Anthony Frasso <afrasso@cimpress.com>
2016-09-30 16:58:54 -0400

    Changing name to 'request-middleware-framework', adding tests and attempting to publish for the first time.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# request-interceptor
+# request-middleware-framework
 
 A framework to intercept HTTP calls made via the request HTTP client.
---
+++ src/test/requestmiddlewareframework.test.js
@@ -0,0 +1,156 @@
+var chai                       = require("chai"),
+    expect                     = require("chai").expect,
+    mockery                    = require("mockery"),
+    spies                      = require("chai-spies"),
+    util                       = require("util"),
+    uuid                       = require("node-uuid"),
+    RequestMiddlewareFramework = require("../index.js");
+
+chai.use(spies);
+
+describe("RequestMiddlewareFramework", function() {
+  var context;
+
+  beforeEach(function() {
+    context = { };
+
+    context.responseBody = uuid.v4();
+    context.mockedRequest = chai.spy(function(uri, options, callback) {
+      if (typeof options === "function" && typeof uri === "object") {
+        callback = options;
+        options = uri;
+      }
+      var response = { body: context.responseBody };
+      callback(null, response, response.body);
+    });
+    context.mockedRequest.defaults = chai.spy(function(requester) {
+      var orig = context.mockedRequest;
+      context.overriddenRequest = chai.spy(function(options, callback) {
+        requester(options, callback);
+      });
+      return context.overriddenRequest;
+    });
+
+    context.middleware = chai.spy(function(options, callback, next) {
+      context.middlewareCallback = chai.spy(function(err, response, body) {
+        callback(err, response, response.body);
+      });
+      next(options, context.middlewareCallback);
+    });
+
+    var rmf = new RequestMiddlewareFramework(context.mockedRequest, context.middleware);
+    context.request = rmf.getMiddlewareEnabledRequest();
+
+    context.options = {
+      uri: `http://${uuid.v4()}`
+    };
+  });
+
+  afterEach(function() {
+    mockery.disable();
+  });
+
+  describe("Executing a request with middleware", function() {
+    it("should execute the middleware appropriately before and after the request is made", function() {
+      context.request(context.options, function(err, response, body) {
+        expect(err).to.not.exist;
+        expect(response).to.exist.and.be.an("object");
+        expect(response).to.have.property("body").and.equal(context.responseBody);
+        expect(body).to.exist.and.be.a("string").and.equal(context.responseBody);
+        expect(context.mockedRequest.defaults).to.have.been.called();
+        expect(context.overriddenRequest).to.have.been.called();
+        expect(context.middleware).to.have.been.called();
+        expect(context.mockedRequest).to.have.been.called();
+        expect(context.middlewareCallback).to.have.been.called();
+      });
+    });
+  });
+
+  describe("Executing a request with multiple middleware passed as arguments", function() {
+    it("should execute the middleware appropriately before and after the request is made", function() {
+      context.middleware2 = chai.spy(function(options, callback, next) {
+        context.middlewareCallback2 = chai.spy(function(err, response, body) {
+          callback(err, response, response.body);
+        });
+        next(options, context.middlewareCallback2);
+      });
+      var rmf = new RequestMiddlewareFramework(context.mockedRequest, context.middleware, context.middleware2);
+      context.request = rmf.getMiddlewareEnabledRequest();
+      context.request(context.options, function(err, response, body) {
+        expect(err).to.not.exist;
+        expect(response).to.exist.and.be.an("object");
+        expect(response).to.have.property("body").and.equal(context.responseBody);
+        expect(body).to.exist.and.be.a("string").and.equal(context.responseBody);
+        expect(context.mockedRequest.defaults).to.have.been.called();
+        expect(context.overriddenRequest).to.have.been.called();
+        expect(context.middleware).to.have.been.called();
+        expect(context.middleware2).to.have.been.called();
+        expect(context.mockedRequest).to.have.been.called();
+        expect(context.middlewareCallback2).to.have.been.called();
+        expect(context.middlewareCallback).to.have.been.called();
+      });
+    });
+  });
+
+  describe("Executing a request with multiple middleware passed as an array", function() {
+    it("should execute the middleware appropriately before and after the request is made", function() {
+      context.middleware2 = chai.spy(function(options, callback, next) {
+        context.middlewareCallback2 = chai.spy(function(err, response, body) {
+          callback(err, response, response.body);
+        });
+        next(options, context.middlewareCallback2);
+      });
+      var rmf = new RequestMiddlewareFramework(context.mockedRequest, [ context.middleware, context.middleware2 ]);
+      context.request = rmf.getMiddlewareEnabledRequest();
+      context.request(context.options, function(err, response, body) {
+        expect(err).to.not.exist;
+        expect(response).to.exist.and.be.an("object");
+        expect(response).to.have.property("body").and.equal(context.responseBody);
+        expect(body).to.exist.and.be.a("string").and.equal(context.responseBody);
+        expect(context.mockedRequest.defaults).to.have.been.called();
+        expect(context.overriddenRequest).to.have.been.called();
+        expect(context.middleware).to.have.been.called();
+        expect(context.middleware2).to.have.been.called();
+        expect(context.mockedRequest).to.have.been.called();
+        expect(context.middlewareCallback2).to.have.been.called();
+        expect(context.middlewareCallback).to.have.been.called();
+      });
+    });
+  });
+
+  describe("Executing a request with undefined middleware", function() {
+    it("should execute not execute any middleware but the request should be executed successfully", function() {
+      var rmf = new RequestMiddlewareFramework(context.mockedRequest);
+      context.request = rmf.getMiddlewareEnabledRequest();
+      context.request(context.options, function(err, response, body) {
+        expect(err).to.not.exist;
+        expect(response).to.exist.and.be.an("object");
+        expect(response).to.have.property("body").and.equal(context.responseBody);
+        expect(body).to.exist.and.be.a("string").and.equal(context.responseBody);
+        expect(context.mockedRequest.defaults).to.have.been.called();
+        expect(context.overriddenRequest).to.have.been.called();
+        expect(context.middleware).to.not.have.been.called();
+        expect(context.mockedRequest).to.have.been.called();
+        expect(context.middlewareCallback).to.not.exist;
+      });
+    });
+  });
+
+  describe("Executing a request with an empty array of middleware", function() {
+    it("should execute not execute any middleware but the request should be executed successfully", function() {
+      var rmf = new RequestMiddlewareFramework(context.mockedRequest, [ ]);
+      context.request = rmf.getMiddlewareEnabledRequest();
+      context.request(context.options, function(err, response, body) {
+        expect(err).to.not.exist;
+        expect(response).to.exist.and.be.an("object");
+        expect(response).to.have.property("body").and.equal(context.responseBody);
+        expect(body).to.exist.and.be.a("string").and.equal(context.responseBody);
+        expect(context.mockedRequest.defaults).to.have.been.called();
+        expect(context.overriddenRequest).to.have.been.called();
+        expect(context.middleware).to.not.have.been.called();
+        expect(context.mockedRequest).to.have.been.called();
+        expect(context.middlewareCallback).to.not.exist;
+      });
+    });
+  });
+});
